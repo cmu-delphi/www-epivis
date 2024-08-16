85a0dd280cf370e6d19674cb3dd07673b2ae79c3
Rostyslav Zatserkovnyi <zatserkovnyi.rostyslav@gmail.com>
2024-08-16 15:42:42 +0300

    Review fixes

--- src/components/dialogs/dataSources/COVIDcast.svelte
+++ src/components/dialogs/dataSources/COVIDcast.svelte
@@ -12,6 +12,8 @@
   let signal = '';
   let geo_type = '';
   let geo_value = '';
+  let form_key = '';
+  let invalid_key = false;
 
   let dataSources: (LabelValue & { signals: string[] })[] = [];
   let geoTypes: string[] = [];
@@ -25,8 +27,10 @@
   }
 
   function fetchMetadata() {
-    fetchCOVIDcastMeta((api_key = api_key)).then((res) => {
+    fetchCOVIDcastMeta((api_key = form_key)).then((res) => {
       if (res.length !== 0) {
+        invalid_key = false;
+        api_key = form_key; // API key is valid -> use it to fetch data later on
         geoTypes = [...new Set(res.map((d) => d.geo_type))];
         const byDataSource = new Map<string, LabelValue & { signals: string[] }>();
         for (const row of res) {
@@ -45,6 +49,8 @@
           entry.signals.sort();
         });
         dataSources = [...byDataSource.values()].sort((a, b) => a.value.localeCompare(b.value));
+      } else {
+        invalid_key = api_key != ''; // mark field as invalid, unless it's empty
       }
     });
   }
@@ -58,7 +64,26 @@
   }
 </script>
 
-<TextField id="{id}-api" label="API Key" name="api_key" on_change={() => fetchMetadata()} bind:value={api_key} />
+<div>
+  <label class="uk-form-label" for="{id}-api">
+    <a href="https://cmu-delphi.github.io/delphi-epidata/api/api_keys.html">API Key</a> (optional)
+  </label>
+  <div class="uk-form-controls">
+    <input
+      id="{id}-api"
+      type="text"
+      class="uk-input"
+      class:uk-form-danger={invalid_key}
+      name="api_key"
+      required={false}
+      bind:value={form_key}
+      on:change={() => fetchMetadata()}
+    />
+    {#if invalid_key}
+      <div class="invalid">API key is invalid - ignoring</div>
+    {/if}
+  </div>
+</div>
 <SelectField id="{id}-r" label="Data Source" name="data_source" bind:value={data_source} options={dataSources} />
 <SelectField id="{id}-r" label="Data Signal" name="signal" bind:value={signal} options={dataSignals} />
 <SelectField id="{id}-gt" label="Geographic Type" bind:value={geo_type} name="geo_type" options={geoTypes} />
@@ -69,3 +94,10 @@
   name="geo_values"
   placeholder="e.g., PA or 42003"
 />
+
+<style>
+  .invalid {
+    color: red;
+    font-size: 0.75rem;
+  }
+</style>
--- src/components/dialogs/inputs/TextField.svelte
+++ src/components/dialogs/inputs/TextField.svelte
@@ -5,12 +5,11 @@
   export let value: string;
   export let placeholder = '';
   export let required = true;
-  export let on_change: () => void = () => {};
 </script>
 
 <div>
   <label class="uk-form-label" for={id}>{label}</label>
   <div class="uk-form-controls">
-    <input type="text" class="uk-input" {name} {required} {id} bind:value on:change={on_change} {placeholder} />
+    <input type="text" class="uk-input" {name} {required} {id} bind:value {placeholder} />
   </div>
 </div>
